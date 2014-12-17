fe3d3a5af0178fe2ecc610dcb2fc0655e31f3713
Michael Platt <michael.platt.07@gmail.com>
2014-12-17 16:57:25 -0500

    Forgot to add the bean to the StickyHeader.xhtml.  Also had to fix the banner click redirect to not include /faces in the URL as it isn't accepted anymore

---
+++ src/java/com/java/beans/StickyHeaderBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.beans;
+
+import com.java.abstractclasses.CommonBeanData;
+import com.java.data.StringConstants;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Singleton;
+
+/**
+ *
+ * @author michael
+ */
+@ManagedBean
+@Singleton
+public class StickyHeaderBean extends CommonBeanData {
+
+    /**
+     * Creates a new instance of StickyHeaderBean
+     */
+    public StickyHeaderBean() {
+        super.stringConstants = new StringConstants();
+    }
+    
+}
